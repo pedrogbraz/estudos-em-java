920d2bc31754ada11b2aafba0de9e5e1d4352cdf
pedrogbraz <pedrobrazdevs@gmail.com>
2024-11-24 18:59:02 -0300

    learning getters and setters

---
+++ src/gettersAndSetters/GettersAndSetters.java
@@ -0,0 +1,11 @@
+package gettersAndSetters;
+
+public class GettersAndSetters {
+	public static void main(String[] args) {
+		Alunos aluno = new Alunos();
+	  aluno.setNome("Pedro Gontijo Braz");
+	  aluno.setIdade(17);
+	  System.out.println(aluno.getNome());
+	  System.out.println(aluno.getIdade());
+	}
+}
